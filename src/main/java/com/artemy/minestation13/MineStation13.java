32ee7d9c60af32dc8ba26982f510ec9a80e44537
Artemy <artemijkarasov6@gmail.com>
2025-05-09 17:28:01 +0300

    Added Items

--- src/main/java/com/artemy/minestation13/MineStation13.java
+++ src/main/java/com/artemy/minestation13/MineStation13.java
@@ -1,17 +1,18 @@
 package com.artemy.minestation13;
 
+import com.artemy.minestation13.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class MineStation13 implements ModInitializer {
-	public static final String MOD_ID = "mine-station-13";
+	public static final String MOD_ID = "mine_station_13";
 
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }
