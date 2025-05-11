cb7ab52814232db4c556469bc25ad4a0306c35b7
Artemy <artemijkarasov6@gmail.com>
2025-05-11 11:34:45 +0300

    Added tooltips

--- src/main/java/com/artemy/minestation13/block/custom/MagicBlock.java
+++ src/main/java/com/artemy/minestation13/block/custom/MagicBlock.java
@@ -6,14 +6,19 @@ import net.minecraft.block.BlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.item.tooltip.TooltipType;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+
+import java.util.List;
 
 public class MagicBlock extends Block {
     public MagicBlock(Settings settings) {
@@ -35,5 +40,10 @@ public class MagicBlock extends Block {
                 itemEntity.setStack(new ItemStack(Items.LAPIS_LAZULI, itemEntity.getStack().getCount()));
             }
         }
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, Item.TooltipContext context, List<Text> tooltip, TooltipType type) {
+        tooltip.add(Text.translatable("tooltip.mine_station_13.magic_block.tooltip"));
     }
 }
