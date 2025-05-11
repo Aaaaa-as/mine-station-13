5e913e6cb41225717f01146baa58f0d1d487d531
Artemy <artemijkarasov6@gmail.com>
2025-05-11 13:12:36 +0300

    Added datagen

---
+++ src/main/java/com/artemy/minestation13/datagen/ModItemTagProvider.java
@@ -0,0 +1,23 @@
+package com.artemy.minestation13.datagen;
+
+import com.artemy.minestation13.item.ModItems;
+import com.artemy.minestation13.util.ModTags;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.minecraft.item.Items;
+import net.minecraft.registry.RegistryWrapper;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModItemTagProvider extends FabricTagProvider.ItemTagProvider {
+    public ModItemTagProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> completableFuture) {
+        super(output, completableFuture);
+    }
+
+    @Override
+    protected void configure(RegistryWrapper.WrapperLookup wrapperLookup) {
+        getOrCreateTagBuilder(ModTags.Items.MAGIC_TRANSFORMABLE_ITEMS)
+                .add(ModItems.RAW_PINK_GARNET)
+                .add(Items.RAW_IRON);
+    }
+}
