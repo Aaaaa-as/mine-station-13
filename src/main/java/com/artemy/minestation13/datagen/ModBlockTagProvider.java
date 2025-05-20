439df49ed8926d6c0a2f08f4c38b33046e0f141a
Aaaaa-as <artemijkarasov6@gmail.com>
2025-05-20 12:56:59 +0300

    Fix tags for new ores

--- src/main/java/com/artemy/minestation13/datagen/ModBlockTagProvider.java
+++ src/main/java/com/artemy/minestation13/datagen/ModBlockTagProvider.java
@@ -22,12 +22,18 @@ public class ModBlockTagProvider extends FabricTagProvider.BlockTagProvider {
                 .add(ModBlocks.RAW_PINK_GARNET_BLOCK)
                 .add(ModBlocks.PINK_GARNET_ORE)
                 .add(ModBlocks.PINK_GARNET_DEEPSLATE_ORE)
+                .add(ModBlocks.PINK_GARNET_NETHER_ORE)
+                .add(ModBlocks.PINK_GARNET_END_ORE)
                 .add(ModBlocks.MAGIC_BLOCK);
+
+        getOrCreateTagBuilder(BlockTags.NEEDS_IRON_TOOL)
+                .add(ModBlocks.PINK_GARNET_NETHER_ORE);
 
         getOrCreateTagBuilder(BlockTags.NEEDS_DIAMOND_TOOL)
                 .add(ModBlocks.RAW_PINK_GARNET_BLOCK)
                 .add(ModBlocks.PINK_GARNET_ORE)
-                .add(ModBlocks.PINK_GARNET_DEEPSLATE_ORE);
+                .add(ModBlocks.PINK_GARNET_DEEPSLATE_ORE)
+                .add(ModBlocks.PINK_GARNET_END_ORE);
 
         getOrCreateTagBuilder(BlockTags.FENCES).add(ModBlocks.PINK_GARNET_FENCE);
         getOrCreateTagBuilder(BlockTags.FENCE_GATES).add(ModBlocks.PINK_GARNET_FENCE_GATE);
