3dbb350e1c51b3e7f5952a3e5cb0cb6a7b3a8f1c
aaaaa-as <artemijkarasov6@gmail.com>
2025-08-22 10:46:01 +0300

    Added variants and sound to Mantis

--- src/main/java/com/artemy/minestation13/entity/client/MantisRenderer.java
+++ src/main/java/com/artemy/minestation13/entity/client/MantisRenderer.java
@@ -2,13 +2,26 @@ package com.artemy.minestation13.entity.client;
 
 import com.artemy.minestation13.MineStation13;
 import com.artemy.minestation13.entity.custom.MantisEntity;
+import com.artemy.minestation13.entity.custom.MantisVariant;
+import com.google.common.collect.Maps;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.render.entity.MobEntityRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.Util;
+
+import java.util.Map;
 
 public class MantisRenderer extends MobEntityRenderer<MantisEntity, MantisModel<MantisEntity>> {
+
+    private static final Map<MantisVariant, Identifier> LOCATION_BY_VARIANT =
+            Util.make(Maps.newEnumMap(MantisVariant.class), map -> {
+                map.put(MantisVariant.DEFAULT,
+                        MineStation13.id("textures/entity/mantis/mantis.png"));
+                map.put(MantisVariant.ORCHID,
+                        MineStation13.id("textures/entity/mantis/mantis_orchid.png"));
+            });
 
 
     public MantisRenderer(EntityRendererFactory.Context context) {
@@ -17,7 +30,7 @@ public class MantisRenderer extends MobEntityRenderer<MantisEntity, MantisModel<
 
     @Override
     public Identifier getTexture(MantisEntity entity) {
-        return MineStation13.id("textures/entity/mantis/mantis.png");
+        return LOCATION_BY_VARIANT.get(entity.getVariant());
     }
 
     @Override
